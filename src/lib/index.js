5e310940f03ee374bdae209c07083f41587c1cd3
Sofya Tuymedova <stuymedova@gmail.com>
2022-06-08 12:27:20 +0300

    Refactor components directory

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,21 +1,26 @@
 // COMPONENTS
-// Controls
-export { Button } from './components/controls/Button'
-export { Link } from './components/controls/Link'
-export { Stepper } from './components/controls/Stepper'
-export { Switch } from './components/controls/Switch'
-export { SegmentedControl, Segment } from './components/controls/SegmentedControl'
+// 1. UI Elements
+// 1.1. Controls and Indicators
+export { Button } from './components/ui-elements/controls-and-indicators/Button'
+export { Link } from './components/ui-elements/controls-and-indicators/Link'
+export { Stepper } from './components/ui-elements/controls-and-indicators/Stepper'
+export { Switch } from './components/ui-elements/controls-and-indicators/Switch'
+export { SegmentedControl, Segment } from './components/ui-elements/controls-and-indicators/SegmentedControl'
 
-// Views
+// 1.2 Presentation
 
-// Modifiers
-export { Wrapper } from './components/modifiers/Wrapper'
-export { VisuallyHidden } from './components/modifiers/VisuallyHidden'
+// 1.3 Utilities
+export { Wrapper } from './components/ui-elements/utilities/Wrapper'
+
+// 2. Layout
+
+// 3. Accessibility
+export { VisuallyHidden } from './components/accessibility/VisuallyHidden'
 
 
 // FUNCTIONS
-// Gestures
+// 1. Gestures
 export { pressOutside } from './functions/gestures/pressOutside'
 export { pressAndHold } from './functions/gestures/pressAndHold'
 
-// Utilities
+// 2. Utilities
